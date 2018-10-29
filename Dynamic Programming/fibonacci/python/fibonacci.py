b5eabbc35cbe11b4fd905b9f1e2eada3ff26e3e6
Ilham Bintang <ilhambintanginformatika@gmail.com>
2018-10-29 22:24:50 +0800

    Update algorithm of fib
    add recursive func to solve fib series with python

--- Dynamic Programming/fibonacci/python/fibonacci.py
+++ Dynamic Programming/fibonacci/python/fibonacci.py
@@ -10,5 +10,15 @@ def fib(n):
 
     return f[(n+1)%2]
 
+startNumber = int(raw_input("Enter the start number here "))
+endNumber = int(raw_input("Enter the end number here "))
+
+def fib_with_recursive(n):
+    if n < 2:
+        return n
+    return fib(n-2) + fib(n-1)
+
+print map(fib_with_recursive, range(startNumber, endNumber))
+        
 n = 9
 print(fib(n))
